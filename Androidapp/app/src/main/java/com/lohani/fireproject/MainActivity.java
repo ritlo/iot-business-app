dfd1cf662c66dc86c41811018d8ef0f77441e6fc
Ritvick Lohani <lohaniritvick@gmail.com>
2022-04-30 12:17:17 +0530

    Inital git repo

---
+++ Androidapp/app/src/main/java/com/lohani/fireproject/MainActivity.java
@@ -0,0 +1,25 @@
+package com.lohani.fireproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().subscribeToTopic("Firetest");
+        Log.d("App opened", "TRUE");
+    }
+}
